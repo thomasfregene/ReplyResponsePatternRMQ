2dc813921cefa9c33c80720cea57795812968f24
thomasfregene <fregzythomas@gmail.com>
2023-12-16 10:33:06 +0100

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Client
+
+var factory = new ConnectionFactory { HostName= "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var replyQueue = channel.QueueDeclare(queue: "", exclusive: true);
+channel.QueueDeclare(queue: "request-queue", exclusive: false);
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Reply Received: {message}");
+};
+
+channel.BasicConsume(queue: replyQueue.QueueName, autoAck: true, consumer: consumer);
+
+//Publish request
+var message = "Can I request a reply";
+var body = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.ReplyTo= replyQueue.QueueName;
+properties.CorrelationId = Guid.NewGuid().ToString();
+
+channel.BasicPublish("", "request-queue", properties, body);
+
+Console.WriteLine($"Sending Request: {properties.CorrelationId}");
+
+Console.WriteLine($"Started Client");
+
+Console.ReadKey();
---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Server
+
+var factory = new ConnectionFactory { HostName= "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare("request-queue", exclusive: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    Console.WriteLine($"Received request: {ea.BasicProperties.CorrelationId}");
+    var replyMessage = $"This is your reply: {ea.BasicProperties.CorrelationId}";
+    var body = Encoding.UTF8.GetBytes(replyMessage);
+
+    channel.BasicPublish("", ea.BasicProperties.ReplyTo, null, body);
+};
+
+channel.BasicConsume(queue: "request-queue", autoAck: true, consumer: consumer);
+
+Console.ReadKey();
